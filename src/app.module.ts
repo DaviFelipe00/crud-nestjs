97286d30ddde63acb149ef9b067fafc8de5e65b5
Davi Barbosa <davi.barbosa@rioave.com.br>
2025-11-27 11:40:57 -0300

    Crud funcional

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // <--- 1. Importe isso
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // 2. Adicione este bloco no TOPO da lista de imports
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o .env disponível no app todo
+    }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,23 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const connectionString = process.env.DIRECT_URL;
+    
+    const pool = new Pool({ 
+      connectionString,
+      // ADICIONE ISTO: O Supabase exige conexão segura
+      ssl: { rejectUnauthorized: false } 
+    });
+    
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
